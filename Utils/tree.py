6fd203ddd981e556f256d05aa6e30299111d976f
yyyeying <henrybai678121@163.com>
2021-07-31 20:06:33 +0800

    给树的实现包装一下

--- Utils/tree.py
+++ Utils/tree.py
@@ -12,7 +12,7 @@ class TreeNode:
         self.right = right
 
 
-def construct_tree(nodes: List[int]):
+def construct_tree(nodes: List[int], debug=False):
     """
     题目没有告诉我他怎么用一个list建的这棵树，为了本地调试只能自己实现一个。
     :param nodes:
@@ -25,12 +25,15 @@ def construct_tree(nodes: List[int]):
     node_to_insert = None
     while count < len(nodes):
         layer_count *= 2
-        # print('{}, {}'.format(count+layer_count, len(nodes)))
+        if debug:
+            print('count+layer_count: {}, nodes: {}'.format(count+layer_count, len(nodes)))
         candidate_nodes = nodes[count:min(count+layer_count, count+len(nodes))]
         candidate_nodes += [None for i in range(count+layer_count - len(nodes))]
-        # print(candidate_nodes)
+        if debug:
+            print('candidate nodes: {}'.format(candidate_nodes))
         for i in range(layer_count):
-            # print("count: {}, layer count: {}, i: {}, node: {}".format(count, layer_count, i, candidate_nodes[i]))
+            if debug:
+                print("count: {}, layer count: {}, i: {}, node: {}".format(count, layer_count, i, candidate_nodes[i]))
             new_node = TreeNode(candidate_nodes[i])
             node_queue.insert(0, new_node)
             if not node_to_insert:
@@ -39,9 +42,11 @@ def construct_tree(nodes: List[int]):
                 node_to_insert.left = new_node
             else:
                 node_to_insert.right = new_node
+                '''
                 if candidate_nodes[i] is None:
                     node_to_insert.left = None
                     node_to_insert.right = None
+                    '''
                 node_to_insert = None
         count += layer_count
     return root
